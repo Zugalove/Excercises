69593ff0bdafafeabe125e8f9810fcb63d39bee8
NDYALIVANAZ <ndyalivanaz@cput.ac.za>
2021-05-07 00:56:27 +0200

    committed to the target folder a new scala code a new code with nested functions added a new variable to file add some more comments

--- src/main/scala/Exercises.scala
+++ src/main/scala/Exercises.scala
@@ -1,32 +1,32 @@
-def main(args: Array[String]): Unit = {
-  // display our an inner class works
-  print(" Hello, Scala , ICE Electives ")
-  val ex = new Exercises()
-  print(3, 4, 5)
-
-}
-
 class Exercises {
+  def main(args: Array[String]) {
+    println(" " + shapes(3, 4, 5))
+  }
+
   //the out class for nested functions
-  def shapes(x: Double, y: Double, z: Double) = {
+  def shapes(x: Double, y: Double, z: Double) {
     // the inner class
     def square(): Unit = {
-      val square = x * x
-      print(" The Area of Square is : " + square)
+
+      print(" The Area of Square is : " + x * x)
+
     }
 
     // the second inner class
-    def triangle(): Unit = {
+    def triangle() {
 
-      val z = 0.
       //added a variable to the formula
-      val triangle = x * y * z
-      print("The Area of Tingle is : " + triangle)
+
+      print("The Area of Tingle is : " + x * y * z)
+
     }
-    // calling the two methods
-    square()
+
+
+    triangle()
     triangle()
 
   }
 
+
 }
+
