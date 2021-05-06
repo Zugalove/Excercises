688dbb3b7ff61245182ad908ad389d4dd3a725c7
NDYALIVANAZ <ndyalivanaz@cput.ac.za>
2021-05-06 23:29:56 +0200

    committed to the target folder a new scala code a new code with nested functions

--- src/main/scala/Exercises.scala
+++ src/main/scala/Exercises.scala
@@ -1,7 +1,30 @@
-class Exercises {
+def main(args: Array[String]): Unit = {
+  // display our an inner class works
+  print(" Hello, Scala , ICE Electives ")
+  val ex = new Exercises()
+  print(3, 4, 5)
 
-  def main(args: Array[String]): Unit = {
-    // our first scala  code
-    print(" Hello, Scala , ICE Electives ")}
+
+}
+
+class Exercises {
+  def shapes(x: Double, y: Double, z: Double) = {
+
+    def square(): Unit = {
+      val square = x * x
+      print(" The Area of Square is : " + square)
+    }
+
+    def triangle(): Unit = {
+
+      val z = 0.5
+      val triangle = x * x * z
+      print("The Area of Tingle is : " + triangle)
+    }
+
+    square()
+    triangle()
+
+  }
 
 }
